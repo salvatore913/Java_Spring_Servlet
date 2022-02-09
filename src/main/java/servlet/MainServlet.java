93377c1ffcd9248dad1f18175db1bfc7ca6ded49
Ivanova Irina <chuda913@ya.ru>
2022-02-09 12:49:10 +0300

    1 commit

---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private final String GET = "GET";
+    private final String POST = "POST";
+    private final String DELETE = "DELETE";
+    private final String PATH = "/api/posts";
+
+    private PostController controller;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET) && path.equals(PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(PATH + "\\d+")) {
+                // easy way
+                final var id = parseId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(PATH + "\\d+")) {
+                // easy way
+                final var id = parseId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    protected long parseId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/")));
+    }
+}
+
